c86a9f1e042a628846984a62c87ea9be414371d1
libermanguy <guyliberman@gmail.com>
2015-10-27 15:27:25 +0200

    a

--- src/boot/Tester.java
+++ src/boot/Tester.java
@@ -7,9 +7,9 @@ public class Tester {
 	public static void main(String[] args) throws Exception {
 			Properties prop = new Properties();
 			
-			prop.saveProp("c:\\temp\\prop.xml");
+			prop.saveProp("c:\\temp\\prop_server.xml");
 			
-			prop.loadProp("c:\\temp\\prop.xml");
+			prop.loadProp("c:\\temp\\prop_server.xml");
 	}
 
 }
